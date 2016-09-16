2f672bcfe00d48df191ef070190cb175bbc9448b
ailtoncsf <ailtoncsf@yahoo.com.br>
2016-09-16 19:26:36 -0300

    Modificando diretório dos arquivos do backend.

--- createEnvironment.sh
+++ createEnvironment.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/viniciustrindade/ubikuos.git
+cd ubikuos/app/server
 virtualenv --system-site-packages --python=python2.7 env
 source env/bin/activate
 pip install flask
